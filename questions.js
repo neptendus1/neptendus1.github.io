936bf3673c830485374aab8c779225d077436e03
neptendus1 <79939010+neptendus1@users.noreply.github.com>
2021-04-24 14:26:34 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,252 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "combien il y a de char tiers 10 dans la nation française?",
+    answer: "3",
+    options: [
+      "3",
+      "5",
+      "7",
+      "4"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Emil1 possède un?",
+    answer: "canon 10.5cm TK 105-g",
+    options: [
+      "canon 12.5cm TK 105-g",
+      "canon 10.5mm GK 105-g",
+      "canon 11.5cm TK 105-g",
+      "canon 10.5cm TK 105-g"
+    ]
+  },
+    {
+    numb: 3,
+    question: "combien il y a de char tiers 8 dans la nation allemand?(sans char premium)",
+    answer: "9",
+    options: [
+      "10",
+      "9",
+      "6",
+      "7"
+    ]
+  },
+    {
+    numb: 4,
+    question: "En tournoi tier 6 les team utilise le plus?",
+    answer: "ARL 44",
+    options: [
+      "Kv2",
+      "ARL v39",
+      "ARL 44",
+      "AT8"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Lequel est un char premium?",
+    answer: "is6",
+    options: [
+      "is4",
+      "is3",
+      "is",
+      "is6"
+    ]
+  },
+      {
+
+    numb: 6,
+
+    question: "Dans la famille des wz lequel est un char premium?",
+    answer: "Wz-120-1G FT",
+    options: [
+      "Wz-131-2G FT",
+      "Wz-111-1G FT",
+      "Wz-120-1G FT",
+      "Wz-111G FT"
+    ]
+  },
+      {
+
+    numb: 7,
+
+    question: "quelle team e-sport à win la finale 2020?",
+    answer: "LGN",
+    options: [
+      "EG",
+      "LOCA",
+      "IMMOR",
+      "LGN"
+    ]
+  },
+      {
+
+    numb: 8,
+
+    question: "combien de nations y a t’il sur wotb?",
+    answer: "8",
+    options: [
+      "10",
+      "6",
+      "8",
+      "1"
+    ]
+  },
+      {
+
+    numb: 9,
+
+    question: "la version Switch est sortie le",
+    answer: "26 août 2020",
+    options: [
+      "10 août 2021",
+      "26 août 2019",
+      "26 août 2020",
+      "20 juin 2020"
+    ]
+  },
+        {
+
+
+
+    numb: 10,
+
+    question: "la version Android est sortie le",
+    answer: "décembre 2014",
+    options: [
+      "décembre 2014",
+      "décembre 2015",
+      "juin 2014",
+      "décembre 2012"
+    ]
+  },
+          {
+
+    numb: 11,
+
+    question: "la version ios est sortie le",
+    answer: "juin 2014",
+    options: [
+      "janvier 2014",
+      "juin 2013",
+      "juin 2014",
+      "décembre 2014"
+    ]
+  },
+          {
+    numb: 12,
+
+    question: "combien de canon possède le t44?",
+    answer: "4",
+    options: [
+      "4",
+      "5",
+      "3",
+      "2"
+    ]
+  },
+          {
+    numb: 13,
+
+    question: "combien de blindage possède le maus sans être anglés?",
+    answer: "200mm",
+    options: [
+      "350mm",
+      "200mm",
+      "400mm",
+      "190mm"
+    ]
+  },
+          {
+    numb: 14,
+
+    question: "En tiers 1 combien il y a de char premium?",
+    answer: "1",
+    options: [
+      "4",
+      "0",
+      "3",
+      "1"
+    ]
+  },
+          {
+    numb: 15,
+
+    question: "Dans la liste une technique n'hésite pas?",
+    answer: "peek-down",
+    options: [
+      "ull-down",
+      "sidescraping",
+      "permatracking",
+      "peek-down"
+      
+    ]
+  },
+  {
+    numb: 16,
+    
+    question: "le t29 a été un projet pour les?",
+    answer: "américain",
+    options: [
+      "Français",
+      "Italien",
+      "américain",
+      "allemand"
+     
+    ]
+  },
+      {
+
+    numb: 17,
+
+    question: "Grille 15 possède un?",
+    answer: "canon 15 cm Pak L/63",
+    options: [
+      "canon 15 mm Pak L/63",
+      "canon 15 cm Pak L/63",
+      "canon 20 cm Pak L/63",
+      "canon 15 cm Pak L/80"
+    ]
+  },
+      {
+
+    numb: 18,
+
+    question: "combien de points structure à le FV215 183?(stock)",
+    answer: "1 800",
+    options: [
+      "1 955",
+      "1 900",
+      "1 855",
+      "1 800"
+    ]
+  },
+      {
+
+    numb: 19,
+
+    question: "le t95 a été un projet pour les?",
+    answer: "américain",
+    options: [
+      "Français",
+      "japonais",
+      "américain",
+      "urss"
+    ]
+  },
+      {
+
+    numb: 20,
+
+    question: "Amx 50B combien il à d'obus dans le barillet?",
+    answer: "3",
+    options: [
+      "2",
+      "4",
+      "6",
+      "3"
+    ]
+  },
+];
